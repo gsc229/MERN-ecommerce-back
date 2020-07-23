f233a3b91b7df8e8b29d6b9c4268197953367529
greg <gsc229@gmail.com>
2020-07-23 08:40:51 -0400

    undid changes

--- routes/order.js
+++ routes/order.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const {requireSignIn, isAuth } = require('../controllers/auth')
 const {create} = require('../controllers/order')
 const {addOrderToUserHistor} = require('../controllers/user')
+const {decreaseQuantity} = require('../controllers/product') // middlware to update the quantity in stock and keep track of the aggregate number of an item sold.
 const { userById } = require('../controllers/user')
 
 
-router.post('/order/create/:userId', requireSignIn, isAuth, addOrderToUserHistor, create)
+router.post('/order/create/:userId', requireSignIn, isAuth, decreaseQuantity ,addOrderToUserHistor, create)
 
 
 router.param('userId', userById)
